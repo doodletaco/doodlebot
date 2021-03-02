7ae6b71906cef4dcb1492c89c2830626a805910c
doodletaco <47832687+doodletaco@users.noreply.github.com>
2021-03-01 23:14:01 -0700

    Reworked commands.

--- main.py
+++ main.py
@@ -8,22 +8,26 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix=":")
+bot = commands.Bot(command_prefix=".")
 
 @bot.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game("with y'all"))
-    print(f'{client.user} has connected to discord!')
+    await bot.change_presence(activity=discord.Game("with y'all"))
+    print(f'{bot.user} has connected to discord!')
 
 @bot.command()
 async def ping(ctx):
-    await ctx.channel.send("pong! :ping_pong:")
+    await ctx.channel.send("pong! <:yall_cute:743358483822280816>")
 
 @bot.command()
-async def echo(ctx, *args):
-    response = ""
-    for arg in args:
-        response = response + " " + arg
-    await ctx.channel.send(response)
+async def echo(ctx):
+    response = ctx.message.content[5:]
+
+    if len(response) > 0:
+        await ctx.channel.send(response)
+    else:
+        await ctx.channel.send("Please stop breaking my bot. <:pandi_screm:727673331758661694>")
+
+    await ctx.message.delete(delay=1)
 
 bot.run(TOKEN)
