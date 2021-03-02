571fd2d36fb982acda32f430a9189aaf50aad5b4
doodletaco <47832687+doodletaco@users.noreply.github.com>
2021-03-01 22:20:23 -0700

    Initial commit

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 # This is a simple discord bot.
 
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix=":")
 
-
-@client.event
+@bot.event
 async def on_ready():
     await client.change_presence(activity=discord.Game("with y'all"))
     print(f'{client.user} has connected to discord!')
 
-client.run(TOKEN)
+@bot.command()
+async def ping(ctx):
+    await ctx.channel.send("pong! :ping_pong:")
+
+@bot.command()
+async def echo(ctx, *args):
+    response = ""
+    for arg in args:
+        response = response + " " + arg
+    await ctx.channel.send(response)
+
+bot.run(TOKEN)
